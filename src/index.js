e34f8f00060262b4d08588531b9df6ee9de15686
Victor R <viictoorrodrigues15@gmail.com>
2022-05-29 14:44:06 -0300

    finalizado

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Home from "./views/Home/Home"; // importação comp Home
-import "./assets/styles/main.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from "./views/Home/Home" // importação comp Home
+import './assets/styles/main.css'; 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    {/* invoca o componente entre tags */}
-    <Home />
-  </React.StrictMode>
-);
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Home from "./views/Home/Home"; // importação comp Home
-import "./assets/styles/main.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* invoca o componente entre tags */}
